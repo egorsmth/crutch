dea854080f80813258e065a23c06bd84f0d160c4
egorsmth <mattspring0@gmail.com>
2019-01-08 12:14:14 +0300

    log error macros added

--- engine/src/core/entryPoint.h
+++ engine/src/core/entryPoint.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#define LOG_ERROR(...) ::Crutch::Log::GetCoreLogger()->error(__VA_ARGS__)
+
 extern Crutch::Application* Crutch::CreateApplication();
 
 int main(int argc, char const *argv[])
 {
     Crutch::Log::Init();
     Crutch::Log::GetCoreLogger()->warn("fucking logger");
+    LOG_ERROR("err");
 
     printf("suck\n");
     auto app = Crutch::CreateApplication();
