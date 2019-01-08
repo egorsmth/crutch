a39bfce52358e7414d967dd532c4e332ab205242
egorsmth <mattspring0@gmail.com>
2019-01-08 12:05:43 +0300

    logger added

--- engine/src/Crutch.h
+++ engine/src/Crutch.h
@@ -2,5 +2,6 @@
 
 #include <stdio.h>
 #include "core/Application.h"
+#include "core/Log.h"
 
 #include "core/entryPoint.h"
--- engine/src/core/entryPoint.h
+++ engine/src/core/entryPoint.h
@@ -4,6 +4,9 @@ extern Crutch::Application* Crutch::CreateApplication();
 
 int main(int argc, char const *argv[])
 {
+    Crutch::Log::Init();
+    Crutch::Log::GetCoreLogger()->warn("fucking logger");
+
     printf("suck\n");
     auto app = Crutch::CreateApplication();
     app->run();
