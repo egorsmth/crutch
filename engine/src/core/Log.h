331234aaad3d86b120e2dc30e637437c9fc05696
egorsmth <mattspring0@gmail.com>
2019-01-08 12:16:59 +0300

    another logger macroses added

--- engine/src/core/Log.h
+++ engine/src/core/Log.h
@@ -16,3 +16,7 @@ namespace Crutch {
         static std::shared_ptr<spdlog::logger> s_ClientLogger;
     };
 }
+
+#define LOG_INFO(...) ::Crutch::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define LOG_DEBUG(...) ::Crutch::Log::GetCoreLogger()->debug(__VA_ARGS__)
+#define LOG_ERROR(...) ::Crutch::Log::GetCoreLogger()->error(__VA_ARGS__)
--- engine/src/core/entryPoint.h
+++ engine/src/core/entryPoint.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#define LOG_ERROR(...) ::Crutch::Log::GetCoreLogger()->error(__VA_ARGS__)
-
 extern Crutch::Application* Crutch::CreateApplication();
 
 int main(int argc, char const *argv[])
@@ -9,6 +7,8 @@ int main(int argc, char const *argv[])
     Crutch::Log::Init();
     Crutch::Log::GetCoreLogger()->warn("fucking logger");
     LOG_ERROR("err");
+    LOG_INFO("err");
+    LOG_DEBUG("err");
 
     printf("suck\n");
     auto app = Crutch::CreateApplication();
