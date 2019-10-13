4fb0c60b661289f43dffdbdc4e568d52f956b009
egorsmth <mattspring0@gmail.com>
2019-10-13 10:09:57 +0300

    events handlers aded

--- shivaEngine/src/core/Application.h
+++ shivaEngine/src/core/Application.h
@@ -2,6 +2,7 @@
 
 #include "core.h"
 #include "events/Event.h"
+#include "events/ApplicationEvent.h"
 #include "Window.h"
 
 namespace Shiva {
@@ -12,7 +13,12 @@ namespace Shiva {
 
         void Run();
 
+        void OnEvent(Event& e);
+
         private:
+
+        bool OnWindowClose(WindowCloseEvent& e);
+
         std::unique_ptr<Window> m_Window;
         bool m_Running = true;
     };
--- shivaEngine/src/platform/unix/UnixWindow.cpp
+++ shivaEngine/src/platform/unix/UnixWindow.cpp
@@ -1,10 +1,17 @@
 #include "UnixWindow.h"
 
 #include "../../core/core.h"
+#include "../../core/events/ApplicationEvent.h"
+#include "../../core/events/MouseEvent.h"
+#include "../../core/events/KeyEvent.h"
 #include "../../core/Log.h"
 
 namespace Shiva {
     static bool s_GLFWInitialized = false;
+
+    static void GLFWErrorCallback(int error, const char* description) {
+        LOG_CORE_ERROR("GLFW Error ({0}): {1}", error, description);
+    }
 
     Window* Window::Create(const WindowProps& props) {
         return new UnixWindow(props);
@@ -29,7 +36,7 @@ namespace Shiva {
         if (!s_GLFWInitialized) {
             int success = glfwInit();
             CORE_ASSERT(success, "Could not initialize GLFW!");
-
+            glfwSetErrorCallback(GLFWErrorCallback);
             s_GLFWInitialized = true;
         }
 
@@ -37,6 +44,99 @@ namespace Shiva {
         glfwMakeContextCurrent(m_Window);
         glfwSetWindowUserPointer(m_Window, &m_Data);
         SetVSync(true);
+
+        // set glfw callbacks
+        glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height) {
+            WindowData& data  = *(WindowData*)glfwGetWindowUserPointer(window);
+            data.Width = width;
+            data.Height = height;
+
+            WindowResizeEvent event(width, height);
+            data.EventCallback(event);
+        });
+
+        glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window) {
+            WindowData& data  = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            WindowCloseEvent event;
+            data.EventCallback(event);
+        });
+
+        glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+            WindowData& data  = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            switch (action)
+            {
+                case GLFW_PRESS:
+                {
+                    KeyPressedEvent event(key, 0);
+                    data.EventCallback(event);
+                    break;
+                }
+                case GLFW_RELEASE:
+                {
+                    KeyReleasedEvent event(key);
+                    data.EventCallback(event);
+                    break;
+                }
+                case GLFW_REPEAT:
+                {
+                    KeyPressedEvent event(key, 1);
+                    data.EventCallback(event);
+                    break;
+                }
+                default:
+                {
+                    LOG_CORE_WARN("glfwSetKeyCallback Invalid action {0}", action);
+                    break;
+                }
+            }
+        });
+
+        glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods) {
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+            
+            switch (action)
+            {
+                case GLFW_PRESS:
+                {
+                    MouseButtonPressedEvent event(button, 0);
+                    data.EventCallback(event);
+                    break;
+                }
+                case GLFW_RELEASE:
+                {
+                    MouseButtonReleasedEvent event(button);
+                    data.EventCallback(event);
+                    break;
+                }
+                case GLFW_REPEAT:
+                {
+                    MouseButtonPressedEvent event(button, 1);
+                    data.EventCallback(event);
+                    break;
+                }
+                default:
+                {
+                    LOG_CORE_WARN("glfwSetMouseButtonCallback Invalid action {0}", action);
+                    break;
+                }
+            }
+        });
+
+        glfwSetScrollCallback(m_Window, [](GLFWwindow* window, double xOffset, double yOffset) {
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            MouseScrolledEvent event((float)xOffset, (float)yOffset);
+            data.EventCallback(event);
+        });
+
+        glfwSetCursorPosCallback(m_Window, [](GLFWwindow* window, double x, double y) {
+            WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+            MouseMovedEvent event((float)x, (float)y);
+            data.EventCallback(event);
+        });
     }
 
     void UnixWindow::ShutDown() {
